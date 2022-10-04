dae5821451571a38223ef4184545d412fb82b7bb
BunoM1234 <101809271+BunoM1234@users.noreply.github.com>
2022-10-04 12:03:34 -0300

    Avance 4/10

--- NovoAPI/NovoApi_APP/models.py
+++ NovoAPI/NovoApi_APP/models.py
@@ -19,3 +19,33 @@ class boards(models.Model):
 	code = models.CharField(max_length=256) # nombre de la columna y tipo. En este caso la columna es code y el tipo es char de limit 256
 	description = models.CharField(max_length=256)# nombre de la columna y tipo. En este caso la columna es code y el tipo es char de limit 256
 	revision = models.IntegerField() # columna revision con tipo int. 
+
+class parts(models.Model):
+	class Meta:
+		db_table = 'parts'
+	name = models.CharField(max_length=150)
+
+
+class part_purchase(models.Model):
+	class Meta:
+		db_table = 'part_purchase'
+
+class bom_parts(models.Model):
+	class Meta:
+		db_table = 'bom_parts'
+
+class suppliers(models.Model):
+	class Meta:
+		db_table = 'suppliers'
+
+class manufacturer(models.Model):
+	class Meta:
+		db_table = 'manufacturer'
+
+class boms(models.Model):
+	class Meta:
+		db_table = 'boms'
+
+class boms(models.Model):
+	class Meta:
+		db_table = 'boms'
